758b6ced037af0acf1a7dc9a7300496b90cb06d0
harrykaestner <kae18001@byui.edu>
2021-06-14 09:52:45 -0600

    update

--- lesson7-2/js/modal.js
+++ lesson7-2/js/modal.js
@@ -1,69 +1,67 @@
-<script type="text/javascript">
-function getCookie(c_name) {
-if(document.cookie.length>0)
-{
-c_start=document.cookie.indexOf(c_name + "=");
-if (c_start!=-1)
-{
-c_start=c_start + c_name.length+1;
-c_end=document.cookie.indexOf(";",c_start);
-if (c_end==-1) c_end=document.cookie.length;
-return unescape(document.cookie.substring(c_start,c_end));
+
+<script type = "text/javascript">
+
+/***********************************************
+* Display time of last visit script
+***********************************************/
+
+var days = 730; // days until cookie expires = 2 years.
+var lastvisit=new Object();
+var firstvisitmsg="This is your first visit to this page. Welcome!"; 
+lastvisit.subsequentvisitmsg="Welcome back visitor! Your last visit was on <b>[displaydate]</b>";
+
+lastvisit.getCookie=function(Name){ 
+var re=new RegExp(Name+"=[^;]+", "i"); 
+if (document.cookie.match(re)) 
+return document.cookie.match(re)[0].split("=")[1];
+return''; 
 }
+
+lastvisit.setCookie=function(name, value, days){ 
+var expireDate = new Date();
+
+var expstring=expireDate.setDate(expireDate.getDate()+parseInt(days));
+document.cookie = name+"="+value+"; expires="+expireDate.toGMTString()+"; path=/";
 }
-return ""; }
-function setCookie(c_name,value,expiredays)
-{
-var exdate=new Date();
-exdate.setDate(exdate.getDate()+expiredays);
-document.cookie=c_name+ "=" +escape(value)+((expiredays==null) ? "" : ";expires="+exdate.toGMTString());
-var x = document.cookie(username);
-alert("sfs");
-document.write(x); }
-function showmessage ()
-{
-var days=421;
-nextvisitmsg="Hi Welcome back ! Your last visit was on [displaydate]";
-var dat=new Date();
-if(getCookie("visit")==""){
-setCookie("visit", dat, days);
-document.write("you are visting this website for the first time ");
+
+lastvisit.showmessage = function() {
+var wh = new Date();
+if (lastvisit.getCookie("visitc") == "") { 
+lastvisit.setCookie("visitc", wh, days); 
+document.write(firstvisitmsg);
 }
-else
-{
-var p=getCookie("visit");
-var pp=Date.parse(p);
+
+else {
+var lv = lastvisit.getCookie("visitc");
+var lvp = Date.parse(lv);
 var now = new Date();
-now.setTime(pp);
+now.setTime(lvp);
 var day = new Array("Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat");
 var month = new Array ("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
-var nd = now.getDate();
-var ny = now.getDay();
-ny = day[ny];
-var nm = now.getMonth();
-nm = month[nm];
+var dd = now.getDate();
+var dy = now.getDay();
+dy = day[dy];
+var mn = now.getMonth();
+mn = month[mn];
 yy = now.getFullYear();
 var hh = now.getHours();
 var ampm = "AM";
-if(hh >= 12) {ampm = "PM"}
+if (hh >= 12) {ampm = "PM"}
+if (hh >12){hh = hh - 12};
+if (hh == 0) {hh = 12}
+if (hh < 10) {hh = "0" + hh};
 var mins = now.getMinutes();
+if (mins < 10) {mins = "0"+ mins}
 var secs = now.getSeconds();
-var dispDate = ny + ", " + nm + " " + nd + ", " + yy + " " + hh + ":" + mins + ":" + secs + " " + ampm;
-document.write(nextvisitmsg.replace("\[displaydate\]", dispDate))
+if (secs < 10) {secs = "0" + secs}
+var dispDate = dy + ", " + mn + " " + dd + ", " + yy + " " + hh + ":" + mins + ":" + secs + " " + ampm
+document.write(lastvisit.subsequentvisitmsg.replace("\[displaydate\]", dispDate))
 }
-setCookie("visit", dat, days); }
-function ctck()
-{
-var sds = document.getElementById("dum");
-if(sds == null){
-alert("You are using a free package.\n You are not allowed to remove the tag.\n");
-}
-var sdss = document.getElementById("dumdiv");
-if(sdss == null){
-alert("You are using a free package.\n You are not allowed to remove the tag.\n");
-} } document.onload="ctck()"; showmessage ();
-</script>
-<div style="font-size: 10px;color: #dadada;" id="dumdiv">
-<a href="https://www.hscripts.com" id="dum" style="text-decoration:none;color: #dadada;">©h</a>
-</div>
 
+lastvisit.setCookie("visitc", wh, days);
+
+}
+
+lastvisit.showmessage();
+
+</script>
