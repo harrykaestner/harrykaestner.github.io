194efb5a37b6ee7304d113f72bd1104d5f5b7b11
harrykaestner <kae18001@byui.edu>
2021-07-02 12:15:48 -0600

    update

--- lesson-11/js/preston.js
+++ lesson-11/js/preston.js
@@ -23,3 +23,27 @@ fetch(url)
       
     }
   });
+
+  const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=739476ad4754b9969a96d0127fc157a7';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    document.getElementById('curr').innerText = jsObject.weather[0].description;
+    document.getElementById('tempH').innerText = jsObject.main.temp_max;
+    document.getElementById('hum').innerText = jsObject.main.humidity;
+    document.getElementById('wSpeed').innerText = jsObject.wind.speed;
+  });
+
+const apiURLforecast = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=739476ad4754b9969a96d0127fc157a7';
+fetch(apiURLforecast)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    const forecast = jsObject['list'];
+
+    for (let i = 3; i < forecast.length; i += 8) {
+      document.getElementById(`imgForecast${i}`).setAttribute('src', "https://openweathermap.org/img/w/" + forecast[i].weather[0].icon + ".png");
+      document.getElementById(`tempForecast${i}`).innerText = forecast[i].main.temp;
+    }
+
+  });
