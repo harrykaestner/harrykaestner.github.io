27957aa8b83462779cf7c4588b9ad4a997470363
harrykaestner <kae18001@byui.edu>
2021-07-02 19:24:35 -0600

    update

--- lesson-11/js/preston.js
+++ lesson-11/js/preston.js
@@ -35,7 +35,7 @@ fetch(apiURL)
     document.getElementById('wSpeed').innerText = jsObject.wind.speed;
   });
 
-const apiURLforecast = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=739476ad4754b9969a96d0127fc157a7';
+const apiURLforecast = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=739476ad4754b9969a96d0127fc157a7';
 fetch(apiURLforecast)
   .then((response) => response.json())
   .then((jsObject) => {
