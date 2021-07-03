0bff1344026620f33f6b98b15aaabe8b005f82c2
harrykaestner <kae18001@byui.edu>
2021-07-02 18:47:20 -0600

    update

--- lesson-11/js/forecast2.js
+++ lesson-11/js/forecast2.js
@@ -1,3 +1,15 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&appid=739476ad4754b9969a96d0127fc157a7';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('curr').innerText = jsObject.weather[0].description;
+    document.getElementById('tempH').innerText = jsObject.main.temp_max;
+    document.getElementById('hum').innerText = jsObject.main.humidity;
+    document.getElementById('wSpeed').innerText = jsObject.wind.speed;
+  });
+
 const weatherURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&appid=739476ad4754b9969a96d0127fc157a7";
 fetch(weatherURL)
   .then((response) => response.json())
