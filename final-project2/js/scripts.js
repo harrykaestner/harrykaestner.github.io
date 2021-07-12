45d579f5bf162f8beabbd00f649e461d35de9b67
harrykaestner <kae18001@byui.edu>
2021-07-12 08:02:16 -0600

    update

--- final-project2/js/scripts.js
+++
@@ -1,8 +0,0 @@
-// Code for responsive navigation
-const hamButton = document.querySelector('#hamBtn');
-const navigation = document.querySelector('.navigation');
-hamButton.addEventListener('click', () => { navigation.classList.toggle('appear') }, false);
-
-//last update date
-let lastUpdated = document.lastModified;
-document.getElementById("updatedDate").textContent = lastUpdated;
