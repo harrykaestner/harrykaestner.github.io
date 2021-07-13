e0cede6f92933678374360fe0aba66a40df4dd09
harrykaestner <kae18001@byui.edu>
2021-07-12 22:48:22 -0600

    update

--- final-project/js/data.js
+++ final-project/js/data.js
@@ -2,9 +2,9 @@
 
 // weather stuff
 async function getWeather() {
-  const API_ID = 'ab6c445f38e58ef35ff191a805f3edc9';
-  const LAT = '43.6488165';
-  const LON = '-116.4058681';
+  const API_ID = '739476ad4754b9969a96d0127fc157a7';
+  const LAT = '43.8231';
+  const LON = '-111.7924';
   const EXCLUDE = 'minutely,hourly';
 
   const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${LAT}&lon=${LON}&exclude=${EXCLUDE}&appid=${API_ID}&units=imperial`;
